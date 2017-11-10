1411703f05de98a20341753b08ca9219eae30d80
VidarUre <vidarure@gmail.com>
2017-11-10 22:32:22 +0100

    La til liste over employees.

--- src/app/contact-employee/contact-employee.component.ts
+++ src/app/contact-employee/contact-employee.component.ts
@@ -8,15 +8,19 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ContactEmployeeComponent implements OnInit {
 
-  EmployeeArray: { name: string, address: string }[] = [
-  { "name": "Hans", "address": "Snarveien 3" },
-  { "name": "Olaug Olaussen", "address": "Melkeveien 1" },
-  { "name": "Harry Potter", "address": "Magiveien 5" }
-];
+  employeeArray = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.employeeArray.push(new Employee("https://images.pexels.com/photos/428341/pexels-photo-428341.jpeg", "Hans Hansen", "Snarveien 3", "123 45 678"));
+    this.employeeArray.push(new Employee("https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg", "Olaug Olaussen", "Melkeveien 1", "987 65 432"));
+    this.employeeArray.push(new Employee("https://images.pexels.com/photos/428331/pexels-photo-428331.jpeg", "Harry Potter", "Magiveien 5", "375 29 485"));
+  }
+}
+
+export class Employee {
+  constructor(public image: string, public name: string, public address: string, public phone: string) {
+  }
 }
