7f87c72a5c56b69bc670a6bf900db43dc6ce04d5
VidarUre <vidarure@gmail.com>
2017-11-10 13:52:00 +0100

    I full gang

--- src/app/contact-employee/contact-employee.component.ts
+++ src/app/contact-employee/contact-employee.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ContactEmployeeComponent implements OnInit {
 
+  EmployeeArray: { name: string, address: string }[] = [
+  { "name": "Hans", "address": "Snarveien 3" },
+  { "name": "Olaug Olaussen", "address": "Melkeveien 1" },
+  { "name": "Harry Potter", "address": "Magiveien 5" }
+];
+
   constructor() { }
 
   ngOnInit() {
